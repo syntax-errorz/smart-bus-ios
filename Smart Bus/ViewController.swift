4aeb794e9b786445d7cbb5bf207c07d8a3c8e98e
Sachin Kumar Singh <singh.s.sachin@icloud.com>
2019-02-14 14:33:19 +0530

    Initial Commit

---
+++ Smart Bus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart Bus
+//
+//  Created by Sachin Kumar Singh on 25/11/1940 Saka.
+//  Copyright © 1940 Sachin Kumar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
